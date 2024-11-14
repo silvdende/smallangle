85d31766f9b52da8aa67c5877213871f6c9f837b
silvdende <silvdende05@gmail.com>
2024-11-14 11:41:27 +0100

    Update smallangle.py
    added click group, commands and options to already existing code

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -3,13 +3,34 @@ import numpy as np
 from numpy import pi
 import pandas as pd
 
+@click.group()
+def cmd_group():
+    pass
 
+
+@cmd_group.command()
+@click.option(
+    "-n",
+    "--number",
+    default=10,
+    help="Number of steps between 0 and 2pi.",
+    show_default=True,  # show default in help
+)
+@click.option("-n", "--number", default=0)
 def sin(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
 
-
+@cmd_group.command()
+@click.option(
+    "-n",
+    "--number",
+    default=10,
+    help="Number of steps between 0 and 2pi.",
+    show_default=True,  # show default in help
+)
+@click.option("-n", "--number", default=0)
 def tan(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
@@ -17,4 +38,4 @@ def tan(number):
 
 
 if __name__ == "__main__":
-    sin(10)
+    cmd_group()
