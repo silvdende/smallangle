d5939e9d26ec7ed7e8b83664ffeec2c81923f4dc
silvdende <silvdende05@gmail.com>
2024-11-14 12:10:57 +0100

    Update smallangle.py
    added docstrings and some comments. default = 10 not 0

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -7,31 +7,38 @@ import pandas as pd
 def cmd_group():
     pass
 
-
+#creates command for sin function
 @cmd_group.command()
 @click.option(
     "-n",
     "--number",
-    default = 0,
+    default = 10,
     help="Number of steps between 0 and 2pi.",
     show_default=True,  # show default in help
 )
 @click.option("-n", "--number", default = 10)
 def sin(number):
+    """gives the sin of NUMBER numbers between 0 and 2pi.
+    
+    NUMBER is the amount of numbers between 0 and 2pi."""
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
 
+#creates command for tan function
 @cmd_group.command()
 @click.option(
     "-n",
     "--number",
-    default = 0,
+    default = 10,
     help="Number of steps between 0 and 2pi.",
     show_default=True,  # show default in help
 )
 @click.option("-n", "--number", default = 10)
 def tan(number):
+    """gives the tan of NUMBER numbers between 0 and 2pi.
+    
+    NUMBER is the amount of numbers between 0 and 2pi."""
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
